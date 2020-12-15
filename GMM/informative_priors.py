cb30bbb01879afd49a3728bd0a894b39ea5b9e53
Ben Gutteridge <gutterboyben@gmail.com>
2020-12-15 19:17:25 +0000

    experimenting with informative priors, first crack

---
+++ GMM/informative_priors.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 15 17:54:19 2020
+
+@author: benpg
+"""
+# External
+import autograd.numpy as np
+from autograd.numpy.random import multivariate_normal
+from autograd import grad
+import matplotlib.pyplot as plt
+plt.close('all')
+from numpy.linalg import inv
+
+# Misc
+from utils import samples, draw_ellipse, plot_GMM, calculate_ELBO, HiddenPrints, cols
+
+# playing with samples
+
+# Define GMMs for generation
+centres = [np.array([0.,3.]), np.array([2.,0.])]
+covs = [np.eye(2), np.array([[0.6,0.4],
+                             [0.4,0.6]])]
+
+K = 2
+
+N_total = 100  # total number of datapoints wanted
+X1 = multivariate_normal(mean=centres[0],
+                         cov=covs[0],
+                         size=int(N_total/2))
+X2 = multivariate_normal(mean=centres[1],
+                         cov=covs[1],
+                         size=int(N_total/2))
+X = np.concatenate((X1,X2))
+
+
+alpha = np.ones(2)*100  # large alpha means pi values are ~=
+beta = np.ones(2)*1000  # large beta keeps Gaussian from which mu is drawn small
+W = [inv(covs[k])/1000 for k in range(K)]
+m = centres
+nu = np.ones(2)*1000
+"""
+nu = d is the least informative prior, and the smallest, so  a large nu
+is more informative. On its own this makes the lam samples huge, so
+we also give W a small prior, the scale - intuitively saying that we are 
+sampling lam from a small space.
+
+"the prior mean of Wishart(W,nu) is nu*W, so a reasonable choice for W would 
+be a prior guess for the precision / nu"
+This is what we have done here, divided W by nu. Nice!
+"""
+
+# the trick to informative priors - 
+# alpha doesnt much matter, apparently
+
+
+title = ''
+for i in range(10):
+    title = '%d'%i
+    Z, pi, mu, lam, r_nk = samples(alpha, beta, W, m, nu, X[0], K) 
+    plot_GMM(X, mu, lam, pi, centres, covs, K, title, cols=['r', 'b'])
+
+    inv_lam_beta = [inv(beta[k]*lam[k]) for k in range(K)]
+    ccols = ['r--', 'b--']
+    for k in range(K):
+        x,y = draw_ellipse(m[k],inv_lam_beta[k])
+        plt.plot(x,y,ccols[k])
