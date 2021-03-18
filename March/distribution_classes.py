fixing covariance GD update eqn and adding plot util function for ellipses of N(\mu_k|m_k,Ck)

March/distribution_classes.py
@@ -10,7 +10,7 @@ from numpy.linalg import inv
 from statistics_of_observed_data import N_k, x_k_bar, S_k
 from CAVI_updates import alpha_k as update_alpha, m_invC_k as update_means_precisions
 from calculate_responsibilities import r_nk
-from grad_funcs import L_grad_alpha, L_grad_m, L_grad_invC
+from grad_funcs import L_grad_alpha, L_grad_m, L_grad_C
 from calculate_ELBO import E_ln_p_X_given_Z_mu, E_ln_p_Z_given_pi, E_ln_p_pi, E_ln_p_mu
 from calculate_ELBO import E_ln_q_Z, E_ln_q_pi, E_ln_q_mu
 
@@ -167,7 +167,7 @@ class VariationalDistribution:
         """
         self.d_alpha = L_grad_alpha(joint.alpha, self.alpha, self.NK) # Xie Eqn 76
         self.d_m = L_grad_m(self.means, joint.mean, joint.precision, self.inv_sigma, self.NK, self.xbar)
-        self.d_invC = L_grad_invC(self.means, self.precisions, self.inv_sigma, joint.precision, self.NK)
+        self.d_C = L_grad_C(self.precisions, self.inv_sigma, joint.precision, self.NK)
 
     def _apply_gradient_updates(self):
         """Applying gradient step updates using calculated gradients and
@@ -176,8 +176,8 @@ class VariationalDistribution:
             # constraints: alpha>0. Setting alpha>.1 as psi'(alpha->0) -> inf
             self.alpha[k] = np.max((self.ALPHA_LB, self.alpha[k] + (self.d_alpha[k]*self.step_sizes['alpha'])))
             self.means[k] = self.means[k] + self.d_m[k]*self.step_sizes['m']
-            self.precisions[k] = self.precisions[k] + self.d_invC[k]*self.step_sizes['invC']
-            self.covariances[k] = inv(self.precisions[k])
+            self.covariances[k] = self.covariances[k] + self.d_C[k]*self.step_sizes['C']
+            self.precisions[k] = inv(self.covariances[k])
 
     def E_step(self, X):
         """

March/grad_funcs.py
@@ -27,14 +27,23 @@ def L_grad_m(m, m0, invC0, invSig, NK, xbar):
         d_m.append((-a-b).reshape(2,))
     return d_m
 
-def L_grad_invC(m, invC, invSig, invC0, NK):
-    K, d_C = len(invC), []
+# # Based on Xie and exponential family stuff - needs another look
+# def L_grad_invC(m, invC, invSig, invC0, NK):
+#     K, d_C = len(invC), []
+#     for k in range(K):
+#         m[k].reshape(2,1)
+#         log_norm = 0.5*multi_dot((m[k].T, invC[k], m[k])) - 0.5*np.log(det(invC[k]))
+#         natural_grad = 0.25*(invC0 + NK[k]*invSig - invC[k]) 
+#         d_C.append(log_norm*natural_grad)
+#     return d_C
+
+def L_grad_C(invC, invSig, invC0, NK):
+    D, K = invSig.shape[0], len(invC)
+    d_C = np.zeros((K,D,D))
     for k in range(K):
-        m[k].reshape(2,1)
-        log_norm = 0.5*multi_dot((m[k].T, invC[k], m[k])) - 0.5*np.log(det(invC[k]))
-        natural_grad = 0.25*(invC0 + NK[k]*invSig - invC[k]) 
-        d_C.append(log_norm*natural_grad)
+        d_C[k] = 0.5*(invC[k] - NK[k]*invSig - invC0)
     return d_C
+    
 
 
 # # cavi update for W, needed for calculating gradient of ELBO wrt Wk

March/run_VI.py
@@ -19,16 +19,16 @@ plt.ioff()
 
 from distribution_classes import JointDistribution, VariationalDistribution
 from generate_dataset import generate_2D_dataset
-from plot_utils import plot_GMM, E_pi, make_gif, plot_1D_phi as plot_1D_param
+from plot_utils import plot_GMM, E_pi, make_gif, plot_1D_phi as plot_1D_param, plot_K_covs, plot_cov_ellipses_gif
 
 # %% Misc setup
 
 "Iterations and update_type"
-K = 3                   # Initial number of mixture components
-N_its = 10              # Number of iterations of chosen update method performed
+K = 4                   # Initial number of mixture components
+N_its = 50              # Number of iterations of chosen update method performed
 
-# update_type = 'GD'      # Using (true) gradient descent
-update_type = 'CAVI'  # Using co-rdinate ascent variational inference algo
+update_type = 'GD'      # Using (true) gradient descent
+# update_type = 'CAVI'  # Using co-rdinate ascent variational inference algo
 
 "Define parameters of joint distribution (effectively priors on variational params)"
 alpha_0 = 1e-3      # Dirichlet prior p(pi) = Dir(pi|alpha0)
@@ -38,14 +38,14 @@ sigma = inv_sigma = np.eye(2)   # Fixed covariance/precision of Gaussian compone
 K_inv_sigma = [inv_sigma for _ in range(K)] # for plotting
 
 "Generating dataset"
-N = 100
-num_clusters = 2
+N = 300
+num_clusters = 3
 X, centres, covs, weights = generate_2D_dataset(N, K=num_clusters, 
                                        # weights=np.random.dirichlet(np.ones(num_clusters)),
                                        weights = np.ones(num_clusters)/num_clusters)
 
 "Schedule for step sizes in GD (constant at the moment)"
-def gd_schedule(step_sizes={'alpha': 1.0, 'm': 1e-2, 'invC': 1e-4}):
+def gd_schedule(step_sizes={'alpha': 1.0, 'm': 1e-2, 'C': 1e-3}):
     # TODO: fill in with a proper schedule
     return step_sizes
 
@@ -83,19 +83,27 @@ for i in tqdm(range(N_its)):
     
 # %% Saving animation, plotting ELBO/other key params
 
-# Make and display gif 
+"Make and display gif of mixture model over time" 
 gifname = make_gif(filedir, gifdir)
 # Empty plots directory for next run
 for file in os.listdir(filedir):
   os.remove(os.path.join(filedir,file))
   
-# xtracting from memory and plotting salient params
+"extracting from memory, and plotting, salient params"
 plt.close('all')
 ELBOs = np.array([variational_memory[n].ELBO for n in range(1,N_its)])
 alphas = np.array([variational_memory[n].alpha for n in range(N_its)])
 mixing_coefficients = np.array([variational_memory[n].mixing_coefficients for n in range(1,N_its)])
+varx = np.array([variational_memory[n].covariances[:,0,0] for n in range(N_its)])
+vary = np.array([variational_memory[n].covariances[:,1,1] for n in range(N_its)])
+covxy = np.array([variational_memory[n].covariances[:,1,0] for n in range(N_its)])
 
 plot_1D_param(ELBOs.reshape(-1,1), 'ELBO', 1)
 plot_1D_param(alphas, 'alphas', K)
 plot_1D_param(mixing_coefficients, 'Mixing coefficients (E[pi_k])', K)
+plot_K_covs(varx,vary,covxy,K)
 plt.show()
+
+"""Makes animation showing the ellipses for the distribution of 
+\mu_k ~ N{\mu_k|m_k, C_k} over time"""
+plot_cov_ellipses_gif(variational_memory, N_its, K, gifname=' --- Mu distribution')